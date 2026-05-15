c71fd7b174333511ddad25419ad240c515ecfb79
Atif Jan <atifjan2019@gmail.com>
2026-05-15 23:52:14 +0500

    Remove fontNastaliq variable from LocaleLayout component className

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -64,7 +64,7 @@ export default async function LocaleLayout({
     <html
       lang={locale}
       dir={dir(locale)}
-      className={`${fontDisplay.variable} ${fontSans.variable} ${fontNastaliq.variable}`}
+      className={`${fontDisplay.variable} ${fontSans.variable}`}
       suppressHydrationWarning
     >
       <body className="flex min-h-dvh flex-col" suppressHydrationWarning>
